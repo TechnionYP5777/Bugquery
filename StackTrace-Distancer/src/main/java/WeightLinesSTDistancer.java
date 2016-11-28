0d958c57ee35ffcd16e81ce256d33802a714e44c
rodedzats <rodedzats@gmail.com>
2016-11-29 00:17:33 +0200

    Added weighted lines implementation

--- StackTrace-Distancer/src/main/java/WeightLinesSTDistancer.java
+++ StackTrace-Distancer/src/main/java/WeightLinesSTDistancer.java
@@ -1,3 +1,4 @@
+import java.util.List;
 
 /**
  * @author rodedzats
@@ -5,9 +6,23 @@
  */
 public class WeightLinesSTDistancer implements StackTraceDistancer {
 	/**
-     * @return returns the jaccard distance, (union_size - intersection_size)
+     * @return returns weighted lines distance between 2 stack traces. simple implementation
+     * TODO: check effectiveness.
      */
     @Override
     public double distance(String o1, String o2) {
+    	List<String> st1 = StackTraceDistancer.splitByNewlines(o1);
+    	List<String> st2 = StackTraceDistancer.splitByNewlines(o2);
+    	double currentWeight = 1;
+    	double currentDistance = 0;
+    	for(int i=0; i < Math.max(st1.size(), st2.size()); i++) {
+    		if(i >= st1.size() || i >= st2.size() || 
+    				!st1.get(i).equalsIgnoreCase(st2.get(i))) {
+    			currentDistance += currentWeight;
+    		}
+    		//TODO: decide how to choose reducing parameter
+    		currentWeight = currentWeight * 0.9;
+    	}
+    	return currentDistance;
     }
 }
