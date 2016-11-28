a723579efb21868e068e7e58d29461447d872e71
rodedzats <rodedzats@gmail.com>
2016-11-28 23:59:21 +0200

    renamed folder to distancer

---
+++ StackTrace-Distancer/src/main/java/WeightLinesSTDistancer.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author rodedzats
+ * @since 28.11.16
+ */
+public class WeightLinesSTDistancer implements StackTraceDistancer {
+	/**
+     * @return returns the jaccard distance, (union_size - intersection_size)
+     */
+    @Override
+    public double distance(String o1, String o2) {
+    }
+}
