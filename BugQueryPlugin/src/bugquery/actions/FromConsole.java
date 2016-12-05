bdcc7746be9548d587b1e527809f123ce800dbe0
yosefraisman <yosefraisman@gmail.com>
2016-12-05 16:31:02 +0200

    Initial Plugin Use-case, Show Console Output Command #12

---
+++ BugQueryPlugin/src/bugquery/actions/FromConsole.java
@@ -0,0 +1,25 @@
+package bugquery.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import bugquery.stacktrace.GetTrace;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+
+public class FromConsole extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent e) throws ExecutionException {
+		
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(e);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"BugQuery",
+				new GetTrace().fromConsole());
+		return null;
+	}
+}
---
+++ BugQueryPlugin/src/bugquery/stacktrace/GetTrace.java
@@ -0,0 +1,55 @@
+package bugquery.stacktrace;
+
+import org.eclipse.ui.console.*;
+
+/**
+ * @author Yosef
+ * @since Nov 29 2016
+ */
+public class GetTrace {
+
+	/**
+	 * @param console_name
+	 * @return a new console, named @console_name.
+	 */
+	private TextConsole emptyNewConsole(String console_name) {
+		MessageConsole new_console = new MessageConsole(console_name, null);
+		ConsolePlugin.getDefault().getConsoleManager().addConsoles(new IConsole[] { new_console });
+		return (TextConsole) new_console;
+	}
+
+	/**
+	 * @return first console within ConsoleManager, or an empty new one if it
+	 *         doesn't exist
+	 */
+	private TextConsole getFirstConsole() {
+		return ConsolePlugin.getDefault().getConsoleManager().getConsoles().length <= 0
+				? emptyNewConsole("BugQuery Error")
+				: (TextConsole) ConsolePlugin.getDefault().getConsoleManager().getConsoles()[0];
+	}
+
+	/**
+	 * @param name_str
+	 *            a subtring of the console's name
+	 * @return a console with a name that has @name_str as a substring within
+	 *         ConsoleManager. if it doesn't exist, returns the first available
+	 *         console, or an empty new one (if it doesn't exist either)
+	 */
+	private TextConsole getConsoleNamed(String name_str) {
+		IConsole[] available = ConsolePlugin.getDefault().getConsoleManager().getConsoles();
+		for (int ¢ = 0; ¢ < available.length; ++¢)
+			if (available[¢].getName().indexOf(name_str) != -1)
+				return (TextConsole) available[¢];
+
+		// no console by that name exists yet
+		return getFirstConsole();
+	}
+
+	/**
+	 * @return console output
+	 */
+	public String fromConsole() {
+		return getConsoleNamed("<terminated>").getDocument().get();
+	}
+
+}
