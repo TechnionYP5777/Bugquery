96e2d052d7dbe6253f27d267a77e21e47920b4a9
Entony Lekhtman <tony.lek1995@gmail.com>
2016-12-07 09:22:11 +0200

    small refactoring fixes #9

--- BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBGetter.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBGetter.java
@@ -1,19 +1,16 @@
 package com.bugquery.serverside.dbparsing;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DBGetter {
-	public Connection getConnection() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+	public void getConnection() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
-	    Connection $ = DriverManager.getConnection("jdbc:mysql://localhost/stackoverflow?user=root&password=1234");
-	    Statement stmt = $.createStatement();
-	    for (ResultSet ¢ = stmt.executeQuery("SELECT * FROM posts"); ¢.next();)
+	    for (ResultSet ¢ = DriverManager.getConnection("jdbc:mysql://localhost/stackoverflow?user=root&password=1234")
+				.createStatement().executeQuery("SELECT * FROM posts"); ¢.next();)
 			System.out.println(¢.getString("Id"));
-	    return $;
+	    
 	}
 	
 	public static void main(String[] args) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
