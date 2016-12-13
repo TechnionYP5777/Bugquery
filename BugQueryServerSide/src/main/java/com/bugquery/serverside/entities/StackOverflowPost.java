fb84b3f4744752d00e5c39cc675561c985966c51
Ziv <szizhar@campus.technion.ac.il>
2016-12-13 19:49:42 +0200

    Fixes in entities, Added tests, Increased test coverage for serverside.stacktrace to 97.6% #27

--- BugQueryServerSide/src/main/java/com/bugquery/serverside/entities/StackOverflowPost.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/entities/StackOverflowPost.java
@@ -1,17 +1,21 @@
 package com.bugquery.serverside.entities;
 
+import java.util.List;
+
+import com.bugquery.serverside.stacktrace.StackTraceExtractor;
+
 public class StackOverflowPost extends Post {
 	public int postTypeId;
-	public int parentId;
-	public int acceptedAnswerId;
+	public String parentId;
+	public String acceptedAnswerId;
 	public int score;
 	public String body;
 	public String title;
 	public String tags;
 	public int answerCount;
-	public StackOverflowPost(String Id, String stackTrace, int postTypeId, int parentId,
-			int acceptedAnswerId, int score ,String body, String title, String tags, int answerCount) {
-		super(Id, stackTrace);
+	public StackOverflowPost(String Id, int postTypeId, String parentId,
+			String acceptedAnswerId, int score ,String body, String title, String tags, int answerCount) {
+		super(Id, getStackTrace(body));
 		this.postTypeId = postTypeId;
 		this.parentId = parentId;
 		this.acceptedAnswerId = acceptedAnswerId;
@@ -20,5 +24,9 @@ public class StackOverflowPost extends Post {
 		this.title = title;
 		this.tags = tags;
 		this.answerCount = answerCount;
+	}
+	//TODO: need to decide which stacktrace to choose, for now, first one.
+	private static String getStackTrace(String body){
+		return StackTraceExtractor.extract(body).get(0).trim();
 	}
 }
