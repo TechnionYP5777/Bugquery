de4a07f700192635144b1e0ca2bf3fe4d7008259
unknown <tony1@csl-gpu7.csf.technion.ac.il>
2016-12-14 12:53:38 +0200

    Now it is working on the servers localhost:4500 #9

--- BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBGetter.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBGetter.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBGetter {
 	public void getConnection() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
-	    for (ResultSet ¢ = DriverManager.getConnection("jdbc:mysql://localhost/stackoverflow?user=root&password=1234")
+	    for (ResultSet ¢ = DriverManager.getConnection("jdbc:mysql://localhost:4500/stackoverflow?user=root&password=root")
 				.createStatement().executeQuery("SELECT * FROM posts"); ¢.next();)
 			System.out.println(¢.getString("Id"));
 	    
--- BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBInitializer.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBInitializer.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 public class DBInitializer {
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException{
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=1234");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:4500?user=root&password=root");
 		
 		Statement st = con.createStatement();
 		
