a4caebc20e5e4fc0bcb360ed60be767169b0cfbb
Entony Lekhtman <tony1@csf.technion.ac.il>
2016-12-20 17:52:56 +0200

    Working on building the db #9

--- BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBGetter.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBGetter.java
@@ -15,22 +15,22 @@ public class DBGetter {
 	    connection.createStatement().executeUpdate("DROP TABLE bugquery_index");
 	    connection.createStatement().executeUpdate("CREATE TABLE bugquery_index(Ex Text,StackTrace Text,Question Text)");
 		for (ResultSet ¢ = connection
-				.createStatement().executeQuery("SELECT * FROM so_posts LIMIT 10000"); ¢.next();){
-	    	String question = ¢.getString("Body");
-	    	
+				.createStatement().executeQuery("SELECT * FROM so_posts LIMIT 100000"); ¢.next();) {
+			String question = ¢.getString("Body");
+			String tags = ¢.getString("Tags");
+			if (tags == null || !tags.toLowerCase().contains("java"))
+				continue;
 			for (StackTrace ¢1 : StackTraceExtractor.extract(question)) {
 				String string = "INSERT INTO bugquery_index(Ex,StackTrace,Question) VALUES(?,?,?)";
 				System.out.println(string);
 				PreparedStatement ps = connection.prepareStatement(string);
 				String ex = (!"".equals(¢1.getException()) ? ¢1.getException() : "No Exception Found!");
-				ps.setString(1,ex );
-				ps.setString(2, ¢1.getStackTrace() );
+				ps.setString(1, ex);
+				ps.setString(2, ¢1.getStackTrace());
 				ps.setString(3, question);
 				ps.executeUpdate();
 			}
-	    	
-	    		
-	    }
+		}
 			
 	    
 	}
