ade685875a996702d3593a02d8bc9b6185716392
Amit Ohayon <amitoha94@gmail.com>
2016-12-21 15:25:17 +0200

    done merging folders, tests pass, website works

--- hello/src/main/java/com/bugquery/serverside/webapp/StackSearchRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bugquery.serverside.webapp;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StackSearchRepository extends CrudRepository<StackSearch, Long> {
-}
