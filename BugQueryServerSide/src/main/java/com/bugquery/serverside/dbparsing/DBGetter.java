9de9a728588d74804e3c6d8dde1c60e0b3163e43
Entony Lekhtman <tony.lek1995@gmail.com>
2016-12-22 01:36:44 +0200

    Refactored the code for db getting and added a method for adding more posts later

--- BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBGetter.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBGetter.java
@@ -19,8 +19,8 @@ public class DBGetter {
 	public static void getConnection() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
 	    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:4488/bugquery?user=root&password=root");
-	    connection.createStatement().executeUpdate("DROP TABLE bugquery_index");
-	    connection.createStatement().executeUpdate("CREATE TABLE bugquery_index(Ex Text,StackTrace Text,Question Text)");
+//	    connection.createStatement().executeUpdate("DROP TABLE bugquery_index");
+	    connection.createStatement().executeUpdate("CREATE TABLE bugquery_index2(Id int,Ex Text,StackTrace Text,Question Text)");
 	    ResultSet r = connection.createStatement().executeQuery("SELECT MAX(Id) as maxId FROM so_posts USE INDEX(Id) LIMIT 10000");
 
 
@@ -35,7 +35,7 @@ public class DBGetter {
 		PrintWriter printer = new PrintWriter( write );
 	    Integer numOfRows = !r.next() ? 0 : r.getInt("maxId");
 	    for  (int i = 0; i <numOfRows;i+=10000){
-	    	System.out.println(i);
+	    	System.out.println(i+1);
 	    	ResultSet rs;
 			for (rs = connection.createStatement()
 					.executeQuery("SELECT * FROM so_posts WHERE Id < " + (i + 10000)+" AND Id > "+i); rs.next();) {
@@ -44,32 +44,65 @@ public class DBGetter {
 				int id = rs.getInt("Id");
 				if (tags == null || !tags.toLowerCase().contains("java"))
 					continue;
-				List<StackTrace> extract = new ArrayList<StackTrace>();
-				try {
-					extract.addAll(StackTraceExtractor.extract(question));
-				} catch (Exception e) {
-					printer.println("The id is: "+id+"\n\n\n\n");
-					printer.println(question+"\n\n\n\n");
-					System.out.println(question+"\n\n\n\n");
-					System.out.println("The id is: "+id+"\n\n\n\n");
-				}
-				for (StackTrace ¢1 : extract) {
-					String string = "INSERT INTO bugquery_index(Ex,StackTrace,Question) VALUES(?,?,?)";
-					System.out.println("The id is: "+id);
-					PreparedStatement ps2 = connection.prepareStatement(string);
-					String ex = ¢1.getException();
-					ps2.setString(1, ex);
-					ps2.setString(2, ¢1.getStackTrace());
-					ps2.setString(3, question);
-					ps2.executeUpdate();
-					ps2.close();
-				}
-				
-				
+				insertQusetionToDB(connection, printer, question, id);
 			}
 			rs.close();
 	    }	
 	    
+	}
+
+	private static void insertQusetionToDB(Connection connection, PrintWriter printer, String question,
+			int id) throws SQLException {
+		List<StackTrace> extract = new ArrayList<StackTrace>();
+		try {
+			extract.addAll(StackTraceExtractor.extract(question));
+		} catch (Exception e) {
+			if (printer == null){
+				System.out.println("Failed with:\n"+ e.getStackTrace());
+			}
+			else{
+				printer.println("The id is: "+id+"\n\n\n\n");
+				printer.println(question+"\n\n\n\n");
+			}
+			
+//			System.out.println(question+"\n\n\n\n");
+//			System.out.println("The id is: "+id+"\n\n\n\n");
+		}
+		for (StackTrace ¢1 : extract) {
+			String string = "INSERT INTO bugquery_index2(Id,Ex,StackTrace,Question) VALUES(?,?,?,?)";
+			System.out.println("The id is: "+id);
+			PreparedStatement ps2 = connection.prepareStatement(string);
+			String ex = ¢1.getException();
+			ps2.setInt(1, id);
+			ps2.setString(2, ex);
+			ps2.setString(3, ¢1.getStackTrace());
+			ps2.setString(4, question);
+			ps2.executeUpdate();
+			ps2.close();
+		}
+	}
+	/**
+	 * 
+	 * @param question The question to be inserted to the db.
+	 * @param logFile If you want to print  to standard input put empty string 
+	 */
+	public static void insertNewExceptionToDB(String question, String logFile) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		try {
+			insertQusetionToDB(
+					DriverManager.getConnection("jdbc:mysql://localhost:4488/bugquery?user=root&password=root"),
+					logFile == "" ? null : new PrintWriter(new FileWriter(logFile)), question, 0);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	    
+		
 	}
 	
 	public static void main(String[] args) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
