7f43b61eee5bedeb5aed7c663127d8e9952b8e11
Entony Lekhtman <tony.lek1995@gmail.com>
2016-12-21 03:58:59 +0200

    Improved code for DBGetter #9

--- BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBGetter.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBGetter.java
@@ -1,5 +1,9 @@
 package com.bugquery.serverside.dbparsing;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -18,37 +22,53 @@ public class DBGetter {
 	    connection.createStatement().executeUpdate("DROP TABLE bugquery_index");
 	    connection.createStatement().executeUpdate("CREATE TABLE bugquery_index(Ex Text,StackTrace Text,Question Text)");
 	    ResultSet r = connection.createStatement().executeQuery("SELECT MAX(Id) as maxId FROM so_posts USE INDEX(Id) LIMIT 10000");
-	    
+
+
+	    String path = "log.txt";
+	    FileWriter write = null;
+		try {
+			write = new FileWriter( path , true);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		PrintWriter printer = new PrintWriter( write );
 	    Integer numOfRows = !r.next() ? 0 : r.getInt("maxId");
-	    PreparedStatement ps = connection.prepareStatement("SELECT * FROM so_posts USE INDEX(Id) WHERE Id=? LIMIT 10000");
-	    for  (int i = 0; i <numOfRows;i+=10000)
-			for (ResultSet r3 = connection.createStatement()
-					.executeQuery("SELECT Id FROM so_posts USE INDEX(Id) LIMIT " + i + "," + (i + 10000)); r3.next();) {
-				ps.setInt(1, r3.getInt("Id"));
-				System.out.println(r3.getInt("Id"));
-				for (ResultSet rs = ps.executeQuery(); rs.next();) {
-					String question = rs.getString("Body");
-					String tags = rs.getString("Tags");
-					if (tags == null || !tags.toLowerCase().contains("java"))
-						continue;
-					List<StackTrace> extract = new ArrayList<StackTrace>();
-					try {
-						extract.addAll(StackTraceExtractor.extract(question));
-					} catch (Exception e) {
-						System.out.println(question);
-					}
-					for (StackTrace ¢1 : extract) {
-						String string = "INSERT INTO bugquery_index(Ex,StackTrace,Question) VALUES(?,?,?)";
-						System.out.println(string);
-						PreparedStatement ps2 = connection.prepareStatement(string);
-						String ex = ¢1.getException();
-						ps2.setString(1, ex);
-						ps2.setString(2, ¢1.getStackTrace());
-						ps2.setString(3, question);
-						ps2.executeUpdate();
-					}
+	    for  (int i = 0; i <numOfRows;i+=10000){
+	    	System.out.println(i);
+	    	ResultSet rs;
+			for (rs = connection.createStatement()
+					.executeQuery("SELECT * FROM so_posts WHERE Id < " + (i + 10000)+" AND Id > "+i); rs.next();) {
+				String question = rs.getString("Body");
+				String tags = rs.getString("Tags");
+				int id = rs.getInt("Id");
+				if (tags == null || !tags.toLowerCase().contains("java"))
+					continue;
+				List<StackTrace> extract = new ArrayList<StackTrace>();
+				try {
+					extract.addAll(StackTraceExtractor.extract(question));
+				} catch (Exception e) {
+					printer.println("The id is: "+id+"\n\n\n\n");
+					printer.println(question+"\n\n\n\n");
+					System.out.println(question+"\n\n\n\n");
+					System.out.println("The id is: "+id+"\n\n\n\n");
 				}
-			}	
+				for (StackTrace ¢1 : extract) {
+					String string = "INSERT INTO bugquery_index(Ex,StackTrace,Question) VALUES(?,?,?)";
+					System.out.println("The id is: "+id);
+					PreparedStatement ps2 = connection.prepareStatement(string);
+					String ex = ¢1.getException();
+					ps2.setString(1, ex);
+					ps2.setString(2, ¢1.getStackTrace());
+					ps2.setString(3, question);
+					ps2.executeUpdate();
+					ps2.close();
+				}
+				
+				
+			}
+			rs.close();
+	    }	
 	    
 	}
 	
