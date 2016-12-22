a6d2980a00eb115898fd8b111fbbe41a55d9868b
ZivIzhar <szizhar@campus.technion.ac.il>
2016-12-22 02:10:02 +0200

    Fixed a test, added exception expectation #43

--- BugQueryServerSide/src/test/java/com/bugquery/serverside/stacktrace/StackTraceRetrieverTest.java
+++ BugQueryServerSide/src/test/java/com/bugquery/serverside/stacktrace/StackTraceRetrieverTest.java
@@ -5,25 +5,46 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.bugquery.serverside.entities.Post;
 import com.bugquery.serverside.entities.StackTrace;
 
 public class StackTraceRetrieverTest {
 
-	@Test
-	public void retrieverReturnsNullForIllegalParameters() {
-	  String stackTrace = "Exception in thread \"main\" java.lang.NullPointerException\n" + 
-        "at com.example.myproject.Book.getTitle(Book.java:16)\n" + 
-        "at com.example.myproject.Author.getBookTitles(Author.java:25)\n" + 
-        "at com.example.myproject.Bootstrap.main(Bootstrap.java:14)";
-		assertNull(StackTraceRetriever.getMostRelevantStackTraces(null, new StackTrace(stackTrace), new JaccardSTDistancer(), 1));
-		assertNull(StackTraceRetriever.getMostRelevantStackTraces(new ArrayList<Post>(), null, new JaccardSTDistancer(), 1));
-		assertNull(StackTraceRetriever.getMostRelevantStackTraces(new ArrayList<Post>(), new StackTrace(stackTrace), null, 1));
-		assertNull(StackTraceRetriever.getMostRelevantStackTraces(new ArrayList<Post>(), new StackTrace(stackTrace), new JaccardSTDistancer(), 0));
+  @Rule public ExpectedException thrown = ExpectedException.none();
+  
+  String stackTrace = "Exception in thread \"main\" java.lang.NullPointerException\n" + 
+      "at com.example.myproject.Book.getTitle(Book.java:16)\n" + 
+      "at com.example.myproject.Author.getBookTitles(Author.java:25)\n" + 
+      "at com.example.myproject.Bootstrap.main(Bootstrap.java:14)";
+  
+  @Test
+	public void retrieverThrowsExceptionForIllegalParameters1() {
+	  thrown.expect(IllegalArgumentException.class);
+		StackTraceRetriever.getMostRelevantStackTraces(null, new StackTrace(stackTrace), new JaccardSTDistancer(), 1);
 	}
 	
+  @Test
+  public void retrieverThrowsExceptionForIllegalParameters2() {
+    thrown.expect(IllegalArgumentException.class);
+    StackTraceRetriever.getMostRelevantStackTraces(new ArrayList<Post>(), null, new JaccardSTDistancer(), 1);
+ }
+
+  @Test
+  public void retrieverThrowsExceptionForIllegalParameters3() {
+    thrown.expect(IllegalArgumentException.class);
+    StackTraceRetriever.getMostRelevantStackTraces(new ArrayList<Post>(), new StackTrace(stackTrace),  null, 1);
+ }
+
+  @Test
+  public void retrieverThrowsExceptionForIllegalParameters4() {
+    thrown.expect(IllegalArgumentException.class);
+    StackTraceRetriever.getMostRelevantStackTraces(new ArrayList<Post>(), new StackTrace(stackTrace), new JaccardSTDistancer(), 0);
+ }
+
 	@Test 
 	public void returnsExactStackTraceIfExists() {
 		String stackTrace1 = "Exception in thread \"main\" java.lang.NullPointerException\n" + 
