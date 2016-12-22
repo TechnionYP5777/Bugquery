de990f2a3aa36e430fddaad2ca8b787aafd5fed3
Entony Lekhtman <tony.lek1995@gmail.com>
2016-12-22 18:58:04 +0200

    Fix to my work after merging problems

--- BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBGetter.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/DBGetter.java
@@ -14,46 +14,43 @@ import java.util.List;
 import com.bugquery.serverside.entities.StackTrace;
 import com.bugquery.serverside.stacktrace.StackTraceExtractor;
 
-@SuppressWarnings("resource")
 public class DBGetter {
-	public static void getConnection() throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+	public static void importFromSOToBugQuery(String logFile) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
 	    try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:4488/bugquery?user=root&password=root")){
-	      ResultSet r = connection.createStatement().executeQuery("SELECT MAX(Id) as maxId FROM so_posts USE INDEX(Id) LIMIT 10000");
-//	    connection.createStatement().executeUpdate("DROP TABLE bugquery_index2");
-//      connection.createStatement().executeUpdate("CREATE TABLE bugquery_index2(Id int,Ex Text,StackTrace Text,Question Text)");
-
-	      String path = "log.txt";
+	      int numOfRows = 0;
+	      try(ResultSet r = connection.createStatement().executeQuery("SELECT MAX(Id) as maxId FROM so_posts USE INDEX(Id) LIMIT 10000")){
+	        if (r.next())
+	          numOfRows = r.getInt("maxId");
+	      }
+	     connection.createStatement().executeUpdate("DROP TABLE bugquery_index2");
+      connection.createStatement().executeUpdate("CREATE TABLE bugquery_index2(Id int,Ex Text,StackTrace Text,Question Text)");
+	     
+	      
 	      FileWriter write = null;
-	    try {
-	      write = new FileWriter( path , true);
-	    } catch (IOException e1) {
-	      // TODO Auto-generated catch block
-	      e1.printStackTrace();
-	    }
-	    PrintWriter printer = new PrintWriter( write );
-	      int numOfRows = !r.next() ? 0 : r.getInt("maxId");
-	      r.close();
-	      for  (int i = 6948892; i <numOfRows;i+=10000){
-	        System.out.println(i+1);
-	        ResultSet rs;
-	      for (rs = connection.createStatement()
-	          .executeQuery("SELECT * FROM so_posts WHERE Id < " + (i + 10000)+" AND Id > "+i); rs.next();) {
-	        String question = rs.getString("Body");
-	        String tags = rs.getString("Tags");
-	        int id = rs.getInt("Id");
-	        if (tags == null || !tags.toLowerCase().contains("java"))
-	          continue;
-	        insertQusetionToDB(connection, printer, question, id);
-	      }
-	      rs.close();
-	      }
-	      printer.close();
-	      connection.close();
-	    }
-
-	    
-	    
+  	    try {
+  	      write = new FileWriter( logFile , true);
+  	    } catch (IOException e1) {
+  	      e1.printStackTrace();
+  	    }
+  	    try(PrintWriter printer = new PrintWriter( write )){
+          for  (int i = 0; i <numOfRows;i+=10000){
+            System.out.println(i+1);
+            try(ResultSet rs = connection.createStatement()
+                .executeQuery("SELECT * FROM so_posts WHERE Id < " + (i + 10000)+" AND Id > "+i)){
+              for (ResultSet i_rs = rs; i_rs.next();) {
+                String question = rs.getString("Body");
+                String tags = rs.getString("Tags");
+                int id = rs.getInt("Id");
+                if (tags == null || !tags.toLowerCase().contains("java"))
+                  continue;
+                insertQusetionToDB(connection, printer, question, id);
+              }
+            }
+            
+          }
+  	    }
+	    }    
 	}
 
 	private static void insertQusetionToDB(Connection c, PrintWriter printer, String question,
@@ -68,23 +65,24 @@ public class DBGetter {
 				printer.println("The id is: "+id+"\n\n\n\n");
 				printer.println(question+"\n\n\n\n");
 			}
-			
-//			System.out.println(question+"\n\n\n\n");
-//			System.out.println("The id is: "+id+"\n\n\n\n");
 		}
+		
 		for (StackTrace ¢1 : extract) {
 			String string = "INSERT INTO bugquery_index2(Id,Ex,StackTrace,Question) VALUES(?,?,?,?)";
 			System.out.println("The id is: "+id);
-			PreparedStatement ps2 = c.prepareStatement(string);
-			String ex = ¢1.getException();
-			ps2.setInt(1, id);
-			ps2.setString(2, ex);
-			ps2.setString(3, ¢1.getString());
-			ps2.setString(4, question);
-			ps2.executeUpdate();
-			ps2.close();
+
+			try(PreparedStatement ps2 = c.prepareStatement(string)){
+  			String ex = ¢1.getException();
+  			ps2.setInt(1, id);
+  			ps2.setString(2, ex);
+  			ps2.setString(3, ¢1.getString());
+  			ps2.setString(4, question);
+  			ps2.executeUpdate();
+			}	
 		}
 	}
+	
+	
 	/**
 	 * 
 	 * @param question The question to be inserted to the db.
@@ -97,11 +95,11 @@ public class DBGetter {
 			e1.printStackTrace();
 		}
 		try {
-			PrintWriter printWriter = new PrintWriter(new FileWriter(logFile));
+			try(PrintWriter printWriter = new PrintWriter(new FileWriter(logFile))){
       insertQusetionToDB(
 					DriverManager.getConnection("jdbc:mysql://localhost:4488/bugquery?user=root&password=root"),
 					logFile == "" ? null : printWriter, question, 0);
-      printWriter.close();
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -112,6 +110,6 @@ public class DBGetter {
 	}
 	
 	public static void main(String[] args) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
-		DBGetter.getConnection();
+		DBGetter.importFromSOToBugQuery("log.txt");
 	}
 }
