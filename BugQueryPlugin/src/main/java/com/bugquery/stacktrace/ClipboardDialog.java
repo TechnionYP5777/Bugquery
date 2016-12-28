d19c7bd798f8ab34db375f89fc2b685d51eae7f5
yosefraisman <yosefraisman@gmail.com>
2016-12-28 12:30:05 +0200

    Changing class ClipboardDialog to Resolve #49

--- BugQueryPlugin/src/main/java/com/bugquery/stacktrace/ClipboardDialog.java
+++ BugQueryPlugin/src/main/java/com/bugquery/stacktrace/ClipboardDialog.java
@@ -3,6 +3,7 @@ package com.bugquery.stacktrace;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.util.Locale;
 
 import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.jface.dialogs.InputDialog;
@@ -23,8 +24,6 @@ import org.eclipse.swt.widgets.Text;
  *
  */
 public class ClipboardDialog extends InputDialog {
-	boolean first = true;
-
 	public ClipboardDialog(Shell parentShell, String dialogTitle, String dialogMessage, String initialValue,
 			IInputValidator validator) {
 		super(parentShell, dialogTitle, dialogMessage, initialValue, validator);
@@ -39,23 +38,26 @@ public class ClipboardDialog extends InputDialog {
 		Text dialog_input = this.getText();
 		((GridData) dialog_input.getLayoutData()).heightHint = 200;
 
-		// Set text to whatever ctrl+v brings
 		dialog_input.addFocusListener(new FocusListener() {
+			boolean first = true;
+			int key_mask = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH).indexOf("mac") >= 0
+					? KeyEvent.VK_META : KeyEvent.VK_CONTROL;
+
 			public void focusLost(FocusEvent __) {
 			}
 
 			public void focusGained(FocusEvent __) {
-				if (first) {
-					Robot robot;
-					try {
-						robot = new Robot();
-						robot.keyPress(KeyEvent.VK_CONTROL);
-						robot.keyPress(KeyEvent.VK_V);
-						robot.keyRelease(KeyEvent.VK_V);
-						robot.keyRelease(KeyEvent.VK_CONTROL);
-					} catch (AWTException e1) {
-						e1.printStackTrace();
-					}
+				if (!first)
+					return;
+				Robot robot;
+				try {
+					robot = new Robot();
+					robot.keyPress(key_mask);
+					robot.keyPress(KeyEvent.VK_V);
+					robot.keyRelease(KeyEvent.VK_V);
+					robot.keyRelease(key_mask);
+				} catch (AWTException e1) {
+					e1.printStackTrace();
 				}
 				first = false;
 			}
