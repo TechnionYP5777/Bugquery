44210b1c17062917c9b1ed31a2e64cf5ebec9887
yonzarecki <szarecki@campus.technion.ac.il>
2017-01-04 18:55:41 +0200

    add stub implementation file for DBConnector #55

---
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/SQLDBConnector.java
@@ -0,0 +1,17 @@
+package com.bugquery.serverside.dbparsing;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.bugquery.serverside.entities.Post;
+
+public class SQLDBConnector implements DBConnector {
+
+	@Override
+	public List<Post> getAllQuestionsWithTheException(String s)
+			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
