4fd5f7ea500ef41c465f905fe2973b32994d438d
Entony Lekhtman <tony.lek1995@gmail.com>
2017-01-10 20:32:21 +0200

    Finished code for importing full db with unique ids

--- BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/dbcreation/DBGetter.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/dbcreation/DBGetter.java
@@ -16,6 +16,8 @@ import com.bugquery.serverside.entities.StackTrace;
 import com.bugquery.serverside.stacktrace.StackTraceExtractor;
 
 public class DBGetter {
+	static int uniqueId;
+	
 	public static void importFromSOToBugQuery(String logFile) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
 	    try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:4488/bugquery?user=root&password=root")){
@@ -24,8 +26,8 @@ public class DBGetter {
 	        if (r.next())
 	          numOfRows = r.getInt("maxId");
 	      }
-//	     connection.createStatement().executeUpdate("DROP TABLE bugquery_index2");
-      connection.createStatement().executeUpdate("CREATE TABLE bugquery_index3(Id int,Ex Text,StackTrace Text,Question Text,PostTypeId int, ParentID int,AcceptedAnswerId int, Score int,Title Text, Tags varchar(500), AnswerCount int)");	      
+	     connection.createStatement().executeUpdate("DROP TABLE bugquery_index3");
+      connection.createStatement().executeUpdate("CREATE TABLE bugquery_index3(BugQueryId int,SOId int,Ex Text,StackTrace Text,Question Text,PostTypeId int, ParentID int,AcceptedAnswerId int, Score int,Title Text, Tags varchar(500), AnswerCount int)");	      
 	      FileWriter write = null;
   	    try {
   	      write = new FileWriter( logFile , true);
@@ -36,21 +38,12 @@ public class DBGetter {
           for  (int i = 0; i <numOfRows;i+=10000){
             System.out.println(i+1);
             try(ResultSet rs = connection.createStatement()
-                .executeQuery("SELECT * FROM so_posts WHERE Id < " + (i + 10000)+" AND Id > "+i)){
+                .executeQuery("SELECT * FROM so_posts USE INDEX(Id) WHERE Id < " + (i + 10000)+" AND Id > "+i)){
               for (ResultSet i_rs = rs; i_rs.next();) {
-                String body = rs.getString("Body");
-                String tags = rs.getString("Tags");
-                int id = rs.getInt("Id");
-                int postTypeId = rs.getInt("PostTypeId");
-                String parentId = rs.getString("ParentId");
-                String acceptedAnswerId = rs.getString("AcceptedAnswerId");
-                int score = rs.getInt("Score");
-                String title = rs.getString("Title");
-                int answerCount = rs.getInt("AnswerCount");
-				StackOverflowPost sop = new StackOverflowPost(Integer.toString(id),postTypeId,parentId, acceptedAnswerId, score,body, title, tags, answerCount);
-                if (tags == null || !tags.toLowerCase().contains("java"))
-                  continue;
-                insertQusetionToDB(connection, printer, sop);
+            	  String tags = rs.getString("Tags");
+            	  if (tags == null || !tags.toLowerCase().contains("java"))
+                      continue;        	  
+            	  insertQusetionToDB(connection, printer, rs);
               }
             }
             
@@ -59,30 +52,49 @@ public class DBGetter {
 	    }    
 	}
 
-	private static void insertQusetionToDB(Connection c, PrintWriter printer, StackOverflowPost p) throws SQLException {
+	private static void insertQusetionToDB(Connection c, PrintWriter printer, ResultSet rs) throws SQLException {
 		List<StackTrace> extract = new ArrayList<>();
+		String body = rs.getString("Body");
+  	  	int id = rs.getInt("Id");
+  	  	int postTypeId = rs.getInt("PostTypeId");
+  	  	int parentId = rs.getInt("ParentId");
+  	  	int acceptedAnswerId = rs.getInt("AcceptedAnswerId");
+  	  	int score = rs.getInt("Score");
+  	  	String title = rs.getString("Title");
+  	  	int answerCount = rs.getInt("AnswerCount");
+  	  	String tags = rs.getString("Tags");
+  	  	
 		try {
-			extract.addAll(StackTraceExtractor.extract(p.body));
+			extract.addAll(StackTraceExtractor.extract(body));
 		} catch (Exception e) {
 			if (printer == null)
-        System.out.println("Failed with:\n" + e.getStackTrace());
-      else{
-				printer.println("The id is: "+p.id+"\n\n\n\n");
-				printer.println(p.body+"\n\n\n\n");
+				System.out.println("Failed with:\n" + e.getStackTrace());
+			else{
+				printer.println("The id is: "+id+"\n\n\n\n");
+				printer.println(body+"\n\n\n\n");
 			}
 		}
 		
 		for (StackTrace ¢1 : extract) {
-			String string = "INSERT INTO bugquery_index3(Id,Ex,StackTrace,Question) VALUES(?,?,?,?)";
-			System.out.println("The id is: "+p.id);
+			String string = "INSERT INTO bugquery_index3(BugQueryId,SOId,Ex,StackTrace,Question,PostTypeId,ParentID,AcceptedAnswerId, Score,Title, Tags, AnswerCount) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
+			System.out.println("The id is: "+id);
 
 			try(PreparedStatement ps2 = c.prepareStatement(string)){
-  			String ex = ¢1.getException();
-  			ps2.setInt(1, Integer.parseInt(p.id));
-  			ps2.setString(2, ex);
-  			ps2.setString(3, ¢1.getString());
-  			ps2.setString(4, p.body);
-  			ps2.executeUpdate();
+	  			String ex = ¢1.getException();
+	  			ps2.setInt(1, uniqueId);
+	  			uniqueId += 1;
+	  			ps2.setInt(2, id);
+	  			ps2.setString(3, ex);
+	  			ps2.setString(4, ¢1.getString());
+	  			ps2.setString(5, body);
+	  			ps2.setInt(6, postTypeId);
+	  			ps2.setInt(7, parentId);
+	  			ps2.setInt(8,acceptedAnswerId);
+	  			ps2.setInt(9, score);
+	  			ps2.setString(10, title);
+	  			ps2.setString(11, tags);
+	  			ps2.setInt(12, answerCount);
+	  			ps2.executeUpdate();
 			}	
 		}
 	}
@@ -93,26 +105,26 @@ public class DBGetter {
 	 * @param question The question to be inserted to the db.
 	 * @param logFile If you want to print  to standard input put empty string 
 	 */
-	public static void insertNewExceptionToDB(StackOverflowPost p, String logFile) {
-		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e1) {
-			e1.printStackTrace();
-		}
-		try {
-			try(PrintWriter printWriter = new PrintWriter(new FileWriter(logFile))){
-      insertQusetionToDB(
-					DriverManager.getConnection("jdbc:mysql://localhost:4488/bugquery?user=root&password=root"),
-					logFile == "" ? null : printWriter, p);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	    
-		
-	}
+//	public static void insertNewExceptionToDB(StackOverflowPost p, String logFile) {
+//		try {
+//			Class.forName("com.mysql.jdbc.Driver").newInstance();
+//		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e1) {
+//			e1.printStackTrace();
+//		}
+//		try {
+//			try(PrintWriter printWriter = new PrintWriter(new FileWriter(logFile))){
+//      insertQusetionToDB(
+//					DriverManager.getConnection("jdbc:mysql://localhost:4488/bugquery?user=root&password=root"),
+//					logFile == "" ? null : printWriter, p);
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	    
+//		
+//	}
 	
 	public static void main(String[] args) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException{
 		DBGetter.importFromSOToBugQuery("log.txt");
