aaad30152e04579569a2bed649be4951c23ea25a
ZivIzhar <szizhar@campus.technion.ac.il>
2017-01-17 15:00:05 +0200

    Added skeletion for DBUpdater #53

---
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/dbupdating/DBUpdater.java
@@ -0,0 +1,22 @@
+package com.bugquery.serverside.dbparsing.dbupdating;
+
+/**
+ * @author zivizhar
+ */
+public class DBUpdater {
+	String server;
+	String XMLPath;
+
+	public DBUpdater(String server, String XMLPath) {
+		this.server = server;
+		this.XMLPath = XMLPath;
+	}
+
+	public void update() {
+		// skeleton for now
+	}
+
+	public void insertXMLFile() {
+		// skeleton for now
+	}
+}
