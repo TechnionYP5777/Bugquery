ce63ad4e20e24b61a961f20ee37ab70525aa779c
Yosef Raisman <yosefraisman@gmail.com>
2017-01-23 15:30:50 +0200

    Basic Solution for #65

--- BugQueryPlugin/src/main/java/com/bugquery/actions/QueryDelegate.java
+++ BugQueryPlugin/src/main/java/com/bugquery/actions/QueryDelegate.java
@@ -4,17 +4,19 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.eclipse.jdt.launching.JavaLaunchDelegate;
 
 /**
  * @author Yosef
  * @since Jan 10 2017
  */
-public class QueryDelegate implements ILaunchConfigurationDelegate {
+public class QueryDelegate extends JavaLaunchDelegate {
 
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
-			throws CoreException {	
+			throws CoreException {
+		super.launch(configuration, mode, launch, monitor);
+		
 	}
 
 }
