6b0d35aca25c2abd732d313f84799624c269102a
ZivIzhar <szizhar@campus.technion.ac.il>
2017-04-01 18:09:38 -0700

    Adding tests for Date case #53

--- BugQueryServerSide/src/test/java/com/bugquery/serverside/dbparsing/dbupdating/XMLDifferTest.java
+++ BugQueryServerSide/src/test/java/com/bugquery/serverside/dbparsing/dbupdating/XMLDifferTest.java
@@ -13,10 +13,25 @@ import org.junit.Test;
 public class XMLDifferTest {
 
 	@Test
-	public void testCreateDiff() {
+	public void testCreateDiffID() {
 		new XMLDiffer(getClass().getClassLoader().getResource("old.xml").getPath(),
 				getClass().getClassLoader().getResource("new.xml").getPath(),
 				getClass().getClassLoader().getResource("").getPath() + "/diff.xml").createDiff(true);
+		try (BufferedReader br1 = new BufferedReader(new FileReader(getClass().getClassLoader().getResource("diffcomp.xml").getPath()));
+				BufferedReader br2 = new BufferedReader(new FileReader(getClass().getClassLoader().getResource("").getPath() + "/diff.xml"))){
+					for (String line1,line2; (line1 = br1.readLine()) != null && (line2 = br2.readLine()) != null;)
+						assertEquals(line1, line2);
+		} catch(Exception ¢){
+			System.out.println(¢.getMessage());
+		}
+
+	}
+	
+	@Test
+	public void testCreateDiffDate() {
+		new XMLDiffer(getClass().getClassLoader().getResource("old.xml").getPath(),
+				getClass().getClassLoader().getResource("new.xml").getPath(),
+				getClass().getClassLoader().getResource("").getPath() + "/diff.xml").createDiff(false);
 		try (BufferedReader br1 = new BufferedReader(new FileReader(getClass().getClassLoader().getResource("diffcomp.xml").getPath()));
 				BufferedReader br2 = new BufferedReader(new FileReader(getClass().getClassLoader().getResource("").getPath() + "/diff.xml"))){
 					for (String line1,line2; (line1 = br1.readLine()) != null && (line2 = br2.readLine()) != null;)
