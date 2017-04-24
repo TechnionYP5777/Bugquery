4592f38be411783e8a30af6f75d324fca2123543
Amit Ohayon <amitoha94@gmail.com>
2017-04-24 13:13:51 +0300

    Moved PostStub to src/test from src/main

---
+++ BugQueryServerSide/src/test/java/com/bugquery/serverside/entities/PostStub.java
@@ -0,0 +1,15 @@
+package com.bugquery.serverside.entities;
+
+/**
+ * @author zivizhar
+ */
+public class PostStub extends Post {
+	public PostStub(StackTrace stackTrace) {
+		super(stackTrace);
+	}
+
+	public PostStub(String stString) {
+		super(stString);
+	}
+
+}
