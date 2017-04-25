71629c4c19139e9f0930486c5e30be5f937b6023
Yossi Gil <yogi@cs.technion.ac.il>
2017-04-25 23:15:20 +0300

    Make the code a bit more elegant

--- BugQueryServerSide/src/main/java/com/bugquery/serverside/stacktrace/GeneralStackTraceRetriever.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/stacktrace/GeneralStackTraceRetriever.java
@@ -14,70 +14,68 @@ import com.bugquery.serverside.exceptions.InvalidStackTraceException;
 
 /**
  * Utility class for getting relevant stack traces from the database
+ * 
  * @author rodedzats
  * @since 11.12.2016
  */
-public class GeneralStackTraceRetriever implements StackTraceRetriever{
-	
+public class GeneralStackTraceRetriever implements StackTraceRetriever {
+
 	private StackTraceDistancer d;
 	private DBConnector connector;
-	
+
 	public GeneralStackTraceRetriever() {
 		this(new JaccardSTDistancer(), new SQLDBConnector());
 	}
-	
+
 	public GeneralStackTraceRetriever(StackTraceDistancer d, DBConnector connector) {
 		this.d = d;
 		this.connector = connector;
 	}
-	
-	/*
-	 * This function returns the posts with the closest stack traces to the input @stackTrace
-	 * using the @distance distancer to sort the traces.
-	 * This function assumes that the database which contains posts with stack traces was
-	 * extracted to a in memory list.
+
+	/**
+	 * This function returns the posts with the closest stack traces to the
+	 * input @stackTrace using the @distance distancer to sort the traces. This
+	 * function assumes that the database which contains posts with stack traces
+	 * was extracted to a in memory list.
 	 */
-	private static List<Post> getMostRelevantStackTraces(List<Post> allPosts, final StackTrace t, StackTraceDistancer d, int numOfPosts) {
-		if(allPosts == null || d == null || numOfPosts <= 0 
-				|| t == null || allPosts.size() <= numOfPosts) // lazy evaluation
+	private static List<Post> getMostRelevantStackTraces(List<Post> allPosts, final StackTrace t, StackTraceDistancer d,
+			int numOfPosts) {
+		if (allPosts == null || d == null || numOfPosts <= 0 || t == null || allPosts.size() <= numOfPosts) // lazy
 			throw new IllegalArgumentException();
-		
-		Comparator<Post> postC = new Comparator<Post>() {
-			@Override
-			public int compare(Post p1, Post p2) {
-				double $ = d.distance(p1.stackTrace, t); // saves time
-				double d_p2 = d.distance(p2.stackTrace, t); // computing
-				return $ > d_p2 ? 1 : $ < d_p2 ? -1 : 0; // distance twice
-			}
-		};
-			
-		PriorityQueue<Post> pq = new PriorityQueue<>(allPosts.size(), postC); 
+
+		final Comparator<Post> comparator = (p1,
+				p2) -> (int) (d.distance(p1.stackTrace, t) - d.distance(p2.stackTrace, t));
+		PriorityQueue<Post> pq = new PriorityQueue<>(allPosts.size(), comparator);
 		pq.addAll(allPosts); // build PQ with the same init size
-		ArrayList<Post> $ = new ArrayList<>();
-		for (int ¢=0;¢<numOfPosts; ++¢)
-			$.add($.size(), pq.poll());
+		List<Post> $ = new ArrayList<>();
+		for (int ¢ = 0; ¢ < numOfPosts; ++¢)
+			$.add(pq.poll());
 		return $;
 	}
 
 	/**
 	 * This function completes the pipeline between the server and the database.
-	 * @param stackTrace - the stack trace which the user queried about
-	 * @param numOfPosts - number of relevant posts needed
+	 * 
+	 * @param stackTrace
+	 *            - the stack trace which the user queried about
+	 * @param numOfPosts
+	 *            - number of relevant posts needed
 	 * @return list of most relevant post to the given stack trace
-	 * @throws GeneralDBException 
-	 * @throws InvalidStackTraceException 
+	 * @throws GeneralDBException
+	 * @throws InvalidStackTraceException
 	 */
 	@Override
-	public List<Post> getMostRelevantPosts(String stackTrace, int numOfPosts) throws GeneralDBException, InvalidStackTraceException {
-		if(stackTrace == null || numOfPosts <= 0)
+	public List<Post> getMostRelevantPosts(String stackTrace, int numOfPosts)
+			throws GeneralDBException, InvalidStackTraceException {
+		if (stackTrace == null || numOfPosts <= 0)
 			throw new IllegalArgumentException();
 		StackTrace $ = new StackTrace(stackTrace);
-		if($.getException() == StackTrace.noExceptionFound)
+		if ($.getException() == StackTrace.noExceptionFound)
 			throw new InvalidStackTraceException("Illegal stack trace ");
 		List<Post> allPosts = new ArrayList<>();
 		try {
 			allPosts = connector.getAllQuestionsWithTheException($.getException());
-		} catch(Exception ¢) {
+		} catch (Exception ¢) {
 			throw new GeneralDBException("General db error: " + ¢.getMessage());
 		}
 		return GeneralStackTraceRetriever.getMostRelevantStackTraces(allPosts, $, d, numOfPosts);
