90d6826e3d81983f5d217680a9ebe5976f35fae2
rodedzats <rodedzats@gmail.com>
2017-04-28 16:34:10 +0300

    Removed todos and opened matching issue #94

--- BugQueryServerSide/src/main/java/com/bugquery/serverside/stacktrace/WeightLinesSTDistancer.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/stacktrace/WeightLinesSTDistancer.java
@@ -9,7 +9,6 @@ import java.util.List;
 public class WeightLinesSTDistancer implements StackTraceDistancer {
 	/**
      * @return returns weighted lines distance between 2 stack traces. simple implementation
-     * TODO: check effectiveness.
      */
     @Override
     public double distance(String o1, String o2) {
@@ -17,12 +16,10 @@ public class WeightLinesSTDistancer implements StackTraceDistancer {
     	List<String> st2 = StackTraceDistancer.splitByNewlines(o2);
     	double currentWeight = 1;
     	double $ = 0;
-    	// TODO better approximation of the levenshtein distance --yg
     	for(int ¢=0; ¢ < Math.max(st1.size(), st2.size()); ++¢) {
     		if(¢ >= st1.size() || ¢ >= st2.size() || 
     				!st1.get(¢).equalsIgnoreCase(st2.get(¢)))
 				$ += currentWeight;
-    		//TODO: decide how to choose reducing parameter
     		currentWeight *= 0.9;
     	}
     	return $;
