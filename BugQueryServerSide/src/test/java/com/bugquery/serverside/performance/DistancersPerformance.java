f148fba194f89895fde67bc0a364bd839691d4a6
ZivIzhar <szizhar@campus.technion.ac.il>
2017-05-09 10:02:13 -0700

    Adding initial performance test files #110 with @rodedzats

---
+++ BugQueryServerSide/src/test/java/com/bugquery/serverside/performance/DistancersPerformance.java
@@ -0,0 +1,16 @@
+package com.bugquery.serverside.performance;
+
+/**
+ * 
+ * @author ZivIzhar
+ * @since 9.5.2017
+ * Check distancer's run performances
+ */
+public class DistancersPerformance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
