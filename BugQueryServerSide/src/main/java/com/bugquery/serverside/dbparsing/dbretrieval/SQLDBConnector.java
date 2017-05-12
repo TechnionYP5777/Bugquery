dd2111e1a90dc58f91b6869fdcca323d25ec7786
Entony Lekhtman <tony.lek1995@gmail.com>
2017-05-12 10:38:21 +0300

    Wrote test for PostRepository, currently not running #102

--- BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/dbretrieval/SQLDBConnector.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/dbparsing/dbretrieval/SQLDBConnector.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import com.bugquery.serverside.entities.MinSOPost;
 import com.bugquery.serverside.entities.Post;
+import com.bugquery.serverside.entities.StackOverflowPost;
 
 /**
  * @author yonzarecki
@@ -22,13 +23,23 @@ public class SQLDBConnector implements DBConnector {
 		List<Post> $ = new ArrayList<>();
 
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:4488/bugquery?user=root&password=root")){
-	  		for (ResultSet ¢ = connection.createStatement().executeQuery("SELECT * FROM bugquery_index2 WHERE Ex='"+s +"'"); 
+		try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/root?user=root&password=root")){
+	  		for (ResultSet ¢ = connection.createStatement().executeQuery("SELECT * FROM bugquery_index3 WHERE Ex='"+s +"'"); 
 	  				¢.next();
 	  				)
-	  			$.add(new MinSOPost(¢.getString("StackTrace"),¢.getString("Question")));
+	  			$.add(new StackOverflowPost(¢.getInt("SOId"), ¢.getInt("PostTypeId"), 
+	  					¢.getInt("ParentId"), ¢.getInt("AcceptedAnswerId"), ¢.getInt("Score"),
+	  					¢.getString("Body"), ¢.getString("Title"), ¢.getString("Tags"), ¢.getInt("AnswerCount")));
+	  			
 		}
 		return $;
+	}
+	
+	
+	public static void main (String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException{
+		for (Post ¢ : new SQLDBConnector().getAllQuestionsWithTheException("java.lang.NullPointerException"))
+			System.out.println(¢.stackTrace.getString());
+		
 	}
 
 }
--- SpringDataExperimentation/src/test/java/com/bugquery/springdataexperimentation/repositories/PostRepositoryTest.java
+++ SpringDataExperimentation/src/test/java/com/bugquery/springdataexperimentation/repositories/PostRepositoryTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,7 +22,7 @@ import com.bugquery.springdataexperimentation.entities.Post;
  * @since May 9, 2017
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
 @Transactional
 public class PostRepositoryTest {
 	@Autowired
@@ -47,5 +48,6 @@ public class PostRepositoryTest {
 		assertThat(returnedList.size(), is(2));
 		assertThat(returnedList.get(0), is(insertedList.get(1)));
 		assertThat(returnedList.get(1), is(insertedList.get(2)));
+		System.out.println(repository.toString());
 	}
 }
