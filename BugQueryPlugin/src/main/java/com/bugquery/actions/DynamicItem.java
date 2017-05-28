224b6bc9bda191fe583834add6c2a04c5a2ac22e
Doron Meshulam <doronmmm@hotmail.com>
2017-05-28 15:18:09 +0300

    close #121

--- BugQueryPlugin/src/main/java/com/bugquery/actions/DynamicItem.java
+++ BugQueryPlugin/src/main/java/com/bugquery/actions/DynamicItem.java
@@ -1,15 +1,26 @@
 package com.bugquery.actions;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.CompoundContributionItem;
+import org.osgi.service.prefs.Preferences;
+
 import com.bugquery.markers.ResourcesUtils;
 
+/**
+ * Dynamic item for choose project menu
+ * 
+ * @author Doron
+ * @since 28 05 2017
+ */
 public class DynamicItem extends CompoundContributionItem {
 
 	public DynamicItem() {
@@ -21,16 +32,17 @@ public class DynamicItem extends CompoundContributionItem {
 
 	@Override
 	public void fill(Menu menu, int index) {
-		// Here you could get selection and decide what to do
-		// You can also simply return if you do not want to show a menu
-
-		// create the menu item
+		Preferences prefs = InstanceScope.INSTANCE.getNode(
+				"com.bugquery.preferences");
 		for (IProject p : ResourcesUtils.getOpenedProjects()) {
 			MenuItem menuItem = new MenuItem(menu, SWT.RADIO, index);
 			menuItem.setText(p.getName());
+			if (prefs.get("projectname", "default").equals(p.getName())) {
+				menuItem.setSelection(true);
+			} 
 			menuItem.addSelectionListener(new SelectionAdapter() {
 				public void widgetSelected(SelectionEvent e) {
-					System.out.println(p.getName());
+					prefs.put("projectname", p.getName());
 				}
 			});
 		}
@@ -38,7 +50,6 @@ public class DynamicItem extends CompoundContributionItem {
 
 	@Override
 	protected IContributionItem[] getContributionItems() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
