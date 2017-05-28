66ac6e0a286fab74d185f521f6a858e924e55bb0
Doron Meshulam <doronmmm@hotmail.com>
2017-05-28 13:48:41 +0300

    #121 open project menu working, now give meaning

--- BugQueryPlugin/src/main/java/com/bugquery/actions/DynamicItem.java
+++ BugQueryPlugin/src/main/java/com/bugquery/actions/DynamicItem.java
@@ -1,17 +1,13 @@
 package com.bugquery.actions;
 
-import java.util.HashMap;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.CompoundContributionItem;
-import org.eclipse.ui.menus.CommandContributionItem;
-import org.eclipse.ui.menus.CommandContributionItemParameter;
-
 import com.bugquery.markers.ResourcesUtils;
 
 public class DynamicItem extends CompoundContributionItem {
@@ -23,48 +19,27 @@ public class DynamicItem extends CompoundContributionItem {
 		super(id);
 	}
 
-	// @Override
-	// public void fill(Menu menu, int index) {
-	// // Here you could get selection and decide what to do
-	// // You can also simply return if you do not want to show a menu
-	//
-	// // create the menu item
-	// MenuItem menuItem = new MenuItem(menu, SWT.RADIO, index);
-	// menuItem.setText(
-	// ResourcesUtils.getOpenedProjects().get(index).getName());
-	// menuItem.addSelectionListener(new SelectionAdapter() {
-	// public void widgetSelected(SelectionEvent e) {
-	// System.out.println("Dynamic menu selected");
-	// }
-	// });
-	// }
+	@Override
+	public void fill(Menu menu, int index) {
+		// Here you could get selection and decide what to do
+		// You can also simply return if you do not want to show a menu
+
+		// create the menu item
+		for (IProject p : ResourcesUtils.getOpenedProjects()) {
+			MenuItem menuItem = new MenuItem(menu, SWT.RADIO, index);
+			menuItem.setText(p.getName());
+			menuItem.addSelectionListener(new SelectionAdapter() {
+				public void widgetSelected(SelectionEvent e) {
+					System.out.println(p.getName());
+				}
+			});
+		}
+	}
 
 	@Override
 	protected IContributionItem[] getContributionItems() {
-		Integer size = ResourcesUtils.getOpenedProjects().size();
-		IContributionItem[] $ = new IContributionItem[size];
-		for (int i = 0; i < size; ++i) {
-			$[i] = createItem(i);
-		}
-		return $;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	private IContributionItem createItem(int index) {
-
-		CommandContributionItemParameter param = new CommandContributionItemParameter(
-				PlatformUI.getWorkbench(), null,
-				"com.bugquery.commands.dynamicitem",
-				CommandContributionItem.STYLE_PUSH);
-//		param.parameters = new HashMap<String, String>();
-//		param.parameters.put("commandParameterID", "TheValue");
-		param.visibleEnabled = true;
-		param.label = ResourcesUtils.getOpenedProjects().get(index).getName(); 
-
-		return new CommandContributionItem(param);
-	}
-
-	@Override
-	public boolean isDynamic() {
-		return true;
-	}
 }
