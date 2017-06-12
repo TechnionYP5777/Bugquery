4278b68e445881c6d05f0312bc6a2cdf3528a5d3
Amit Ohayon <amitoha94@gmail.com>
2017-06-12 17:37:51 +0300

    updated tests for new stachsearch behavior

--- BugQueryServerSide/src/test/java/com/bugquery/serverside/entities/StackSearchTest.java
+++ BugQueryServerSide/src/test/java/com/bugquery/serverside/entities/StackSearchTest.java
@@ -2,9 +2,12 @@ package com.bugquery.serverside.entities;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 import org.junit.Test;
+
+import com.bugquery.serverside.exceptions.InvalidStackTraceException;
 
 /**
  * 
@@ -17,7 +20,26 @@ public class StackSearchTest {
 	@Test
 	@SuppressWarnings("static-method")
 	public void shouldDecodeTrace() throws Exception {
-		assertThat((new StackSearch(URLEncoder.encode("decoded trace", "UTF-8"))).getTrace())
-				.isEqualTo("decoded trace");
+		String legalTrace = "Exception in thread \"main\" java.lang.NullPointerException\n" + 
+				"        at com.example.myproject.Book.getTitle(Book.java:16)\n" + 
+				"        at com.example.myproject.Author.getBookTitles(Author.java:25)\n" + 
+				"        at com.example.myproject.Bootstrap.main(Bootstrap.java:14)";
+		assertThat(getEncodedStackSearch(legalTrace).getTrace())
+				.isEqualTo(legalTrace);
+	}
+	
+	@Test(expected=InvalidStackTraceException.class)
+	public void shouldThrowForInvalidTrace() throws Exception {
+		getEncodedStackSearch("hi");
+	}
+	
+	private StackSearch getEncodedStackSearch(String content) throws InvalidStackTraceException {
+		try {
+			return new StackSearch(URLEncoder.encode(content, "UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			// Can't happen
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
