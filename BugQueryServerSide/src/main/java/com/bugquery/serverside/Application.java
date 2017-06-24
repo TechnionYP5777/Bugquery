d0660cb156b111250f0720c2d4994bce81febb4f
Amit Ohayon <amitoha94@gmail.com>
2017-06-24 22:18:27 +0300

    Setting up a new service for updating DB which doesn't run unless specified on command line - #173

--- BugQueryServerSide/src/main/java/com/bugquery/serverside/Application.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/Application.java
@@ -1,14 +1,13 @@
 package com.bugquery.serverside;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 import com.bugquery.serverside.examples.ExamplesXMLCreator;
+import com.bugquery.serverside.services.StackOverflowService;
 
 /**
  * here everything starts, default class for running spring boot application
@@ -18,30 +17,27 @@ import com.bugquery.serverside.examples.ExamplesXMLCreator;
  *
  */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 	
+	@Autowired
+	StackOverflowService soService;
+
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
-	
+
 	public static void main(String[] args) {
 		if (args.length > 0 && "--createExamples".equals(args[0]))
 			ExamplesXMLCreator.activate();
-		SpringApplication.run(Application.class,args);
+		SpringApplication.run(Application.class, args);
 	}
-	
-	// Has to be non-static, doesn't work otherwise!
-	@Bean
-	@SuppressWarnings("static-method")
-	public CommandLineRunner checkDBargs() {
-		return args -> {
-			
-			if (args.length > 0) {
-				if ("--createDB".equals(args[0]))
-					log.info("Created DB");
-				
-				if ("--updateDB".equals(args[0]))
-					log.info("Updated DB");
+
+	@Override
+	public void run(String... args) {
+		if (args.length > 0) {
+			if ("--updateDB".equals(args[0])) {
+				soService.updatePosts();
+				log.info("Updated DB");
 			}
-		};
+		}
 	}
 
 }
---
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/services/StackOverflowService.java
@@ -0,0 +1,10 @@
+package com.bugquery.serverside.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StackOverflowService {
+	public static void updatePosts() {
+		throw new UnsupportedOperationException("Updating is not yet implemented");
+	}
+}
