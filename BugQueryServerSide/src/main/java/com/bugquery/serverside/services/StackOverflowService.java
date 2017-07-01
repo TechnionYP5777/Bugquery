308718d0f5fc836469350ea5aa9bb843055126fc
Yonzarecki <szarecki@campus.technion.ac.il>
2017-07-01 19:57:18 +0300

    #165 - @AmitOhayon

--- BugQueryServerSide/src/main/java/com/bugquery/serverside/services/StackOverflowService.java
+++ BugQueryServerSide/src/main/java/com/bugquery/serverside/services/StackOverflowService.java
@@ -1,11 +1,9 @@
 package com.bugquery.serverside.services;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,9 +30,6 @@ public class StackOverflowService {
 	private JdbcTemplate jdbcTemplate;
 	private PostRepository repo;
 
-	private static final String COM_MYSQL_JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	private static final String DB_ADDRESS = "localhost:4499";
-
 	@Autowired
 	public StackOverflowService(ApplicationArguments args, PostRepository repo, JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
@@ -44,10 +39,8 @@ public class StackOverflowService {
 	}
 
 	public void updatePosts(String xmlLocation){
-		//importStackOverflowDB(xmlLocation);
+		importStackOverflowDB(xmlLocation);
 		addToPosts();
-
-		throw new UnsupportedOperationException("Updating is not yet implemented");
 	}
 
 	private void addToPosts() {
@@ -58,15 +51,12 @@ public class StackOverflowService {
 				String body = rs.getString("Body");
 				for (StackTrace stackTrace : StackTraceExtractor.extract(body)) {
 					Post p = new Post(stackTrace);
-					//TODO: check if answerAdding is working
 					int acceptedAnswerId = rs.getInt("AcceptedAnswerId");
 					if (rs.wasNull()){
 						p.setAnswer(null);
 					} else{
 						p.setAnswer(""+acceptedAnswerId);
 					}
-					//String answer = jdbcTemplate.queryForObject("SELECT Body FROM so_posts WHERE Id = ?", String.class, acceptedAnswerId);
-					p.setAnswer(""+acceptedAnswerId);
 					p.setQuestion(body);
 					p.setTitle(rs.getString("Title"));
 					results.add(p);
@@ -80,6 +70,7 @@ public class StackOverflowService {
 			if  (p.getAnswerId() != null){
 				String answer = jdbcTemplate.queryForObject("SELECT Body FROM so_posts WHERE Id = ?", String.class, p.getAnswerId());
 				p.setAnswer(answer);
+				repo.save(p);
 			}
 		}
 	}
@@ -91,38 +82,9 @@ public class StackOverflowService {
 	private void importStackOverflowDB(String xmlLocation) {
 		jdbcTemplate.execute("DROP TABLE IF EXISTS so_posts");
 		jdbcTemplate.execute(
-				"CREATE TABLE so_posts(Id int, AcceptedAnswerId int, Body Text, Title Text, Tags varchar(500)) ENGINE = MYISAM");
+				"CREATE TABLE so_posts(Id int PRIMARY KEY, AcceptedAnswerId int, Body Text, Title Text, Tags varchar(500))");
 		jdbcTemplate.execute(
 				"LOAD XML INFILE \"" + xmlLocation + "\" INTO TABLE so_posts(Id, AcceptedAnswerId, Body, Title, Tags)");		
-	}
-
-	public void importAnswersFromStackOverflow() throws Exception {
-		Class.forName(COM_MYSQL_JDBC_DRIVER).newInstance();
-		try (Connection conn = DriverManager
-				.getConnection("jdbc:mysql://" + DB_ADDRESS + "/bugquery?user=root&password=root")) {
-			int c = (int) repo.count();
-			for (Long i = 1L; i <= c; ++i) {
-				Post p = repo.findOne(i);
-				p.setAnswer(getAnswerFromAnswerId(conn, p.getAnswerId()));
-				repo.save(p);
-				System.out.println(i);
-			}
-		}
-	}
-
-	private static String getAnswerFromAnswerId(Connection c, String answerId) {
-
-		String answer = null;
-		if ("0".equals(answerId))
-			return null;
-		try (ResultSet rs = c.createStatement()
-				.executeQuery("SELECT * FROM so_posts USE INDEX(Id) WHERE Id = " + answerId)) {
-			for (ResultSet i_rs = rs; i_rs.next();)
-				answer = i_rs.getString("Body");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return answer;
 	}
 	
 	class StreamingStatementCreator implements PreparedStatementCreator {
